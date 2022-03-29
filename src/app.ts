e6f9e3d6fe8a520e320b48c6f02cf59dc6daae3c
Matheus Nunes Boraso <matheusnunesbrasil@outlook.com>
2022-03-29 14:36:49 -0300

    feat: modules

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,32 @@ import logged from "./functions/logged";
 import erAgendas from "./routes/agenda";
 import erError from "./routes/error";
 import rtRegistro from "./routes/registro";
+import moment from "moment-timezone";
 
 export const prisma = new PrismaClient();
 export const app = express();
 
 app.use(express.json());
+
+app.get(`/:what`, async (_req, res) => {
+  const data = moment().locale("pt-br").tz("America/Sao_Paulo").format("L");
+  const time = moment().locale("pt-br").tz("America/Sao_Paulo").format("LT");
+  const resultLogged = await logged(_req.headers);
+  console.log("LOGADO ->", resultLogged);
+  res.json([data, time]);
+});
+
+app.get(`/:what/:second`, async (_req, res) => {
+  res.redirect(200, `/${_req.params.what}/${_req.params.second}`);
+  const data = moment().locale("pt-br").tz("America/Sao_Paulo").format("L");
+  const time = moment().locale("pt-br").tz("America/Sao_Paulo").format("LT");
+  const resultLogged = await logged(_req.headers);
+  console.log("LOGADO ->", resultLogged);
+  res.json([data, time, "second"]);
+});
+
 app.use("/agendas", erAgendas);
 app.use("/registro", rtRegistro);
 app.use("/error", erError);
-
-app.get(`/:what`, async (_req, res) => {
-  const resultLogged = await logged(_req.headers);
-  console.log("LOGADO ->", resultLogged);
-  res.json([resultLogged]);
-});
 
 export default app;
--- src/routes/registro.ts
+++ src/routes/registro.ts
@@ -1,18 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import logged from "../functions/logged";
 
 const rtRegistro = express.Router();
 const prisma = new PrismaClient();
 
 // er - ExpressRouter.
 //
-// Procurar todos
+// Procurar todos registros
 rtRegistro.get(`/`, async (req, res) => {
   try {
+    if (!!logged(req.headers)) {
+      throw new Error("Usuário não logado");
+    }
     const results = prisma.net_registropedagogico.findMany();
     res.json(results);
   } catch (error) {
-    res.status(500).end();
+    if (error instanceof Error) {
+      res.status(500).send({ error: error.message });
+    }
   }
 });
 
